8bab442703cf16c05059eb13db0a2ec4b7298586
LeoRey <reikardnapitupulu@gmail.com>
2022-08-19 06:47:01 +0700

    Reikard

---
+++ IngameShop/models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const bycrypt = require('bcryptjs')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(models.UserDetail, {foreignKey: 'UserId'})
+    }
+  }
+  User.init({
+    email: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      
+    },
+    role: DataTypes.BOOLEAN
+  }, {
+    hooks:{
+      beforeCreate: function (User){
+        User.role = false
+        const salt = bycrypt.genSaltSync(5)
+        User.password = bycrypt.hashSync(User.password,  salt)
+      }
+    },
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
