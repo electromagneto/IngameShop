7b4ee91f8611bca485ee511abf26eb42b8d98548
electromagneto <amru.mufid@gmail.com>
2022-08-19 10:37:24 +0700

    Migrations

--- IngameShop/controllers/index.js
+++ IngameShop/controllers/index.js
@@ -1,49 +1,52 @@
 'use strict';
 
-const { User, Profile, Item, Transaction } = require('../models')
+const { User, Profile, Item, Transaction } = require('../models');
 
-class Controller{
-    static home(req,res){
-        Item.findAll()
-        .then((result) =>{
-            res.render('home.ejs', {result});
-        })
-        .catch((err) =>{
-            res.send(err);
-        });
-    }
+class Controller {
+  static home(req,res){
+    Item.findAll()
+    .then((result) =>{
+      res.render('home.ejs', {result});
+    })
+    .catch((err) =>{
+      res.send(err);
+    });
+  }
 
-    static userById(req,res) {
-        
-    }
+  static userById(req,res) {
+      
+  }
 
-    static profileById(req,res) {
+  static profileById(req,res) {
 
-    }
-    
-    static loginForm(req, res){
-        res.render('loginForm.ejs');
-    }
-    static postLogin(req,res){
-        const {email, password} = req.body;
-        User.findOne({where: {email: `${email}`}})
-    }
-    static registerForm(req, res){
-        res.render('registerForm.ejs')
-    }
-    static postRegister(req,res){
-        const {name, password, email, profilePicture} = req.body;
-        User.create({email,password})
-        .then((result) =>{
-            Profile.create({name, profilePicture, UserId: result.id})
-        })
-        .then(() =>{
-            res.redirect('/login');
-        })
-        .catch((err) =>{
-            res.send(err);
-        });
-    }    
+  }
+  
+  static loginForm(req, res){
+    res.render('loginForm.ejs');
+  }
+  
+  static postLogin(req,res){
+    const {email, password} = req.body;
+    User.findOne({where: {email: `${email}`}})
+  }
+
+  static registerForm(req, res){
+    res.render('registerForm.ejs');
+  }
+
+  static postRegister(req,res){
+    const {name, password, email, profilePicture} = req.body;
+    User.create({email,password})
+    .then((result) =>{
+      Profile.create({name, profilePicture, UserId: result.id})
+    })
+    .then(() =>{
+      res.redirect('/login');
+    })
+    .catch((err) =>{
+      res.send(err);
+    });
+  }    
 }
 
 module.exports = Controller
